b87edd34c189554ded53779d75e2e550b198a963
amit <amit1schneider@gmail.com>
2024-11-25 17:52:49 +0200

    calculator

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.simplecalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    // Get view handle using the view id
+
+
+    public void onBtnClicked(View view) {
+        EditText et1 = findViewById(R.id.Num1);
+
+        // Get the view’s text
+        String et1Text = et1.getText().toString();
+
+        // Cast the String into Integer
+        Integer num1 = Integer.valueOf(et1Text);
+        Integer result = null;
+        Integer num2;
+        if (view.getId() == R.id.btnPlus)
+            result = num1 + num2;
+        if (view.getId() == R.id.btnMinus)
+            result = num1 - num2;
+        if (view.getId() == R.id.btnMult)
+            result = num1 * num2;
+        if (view.getId() == R.id.btnDiv)
+            result = num1 / num2;
+        if (result != null) {
+            TextView tvResult = findViewById(R.id.tvResult);
+            tvResult.setText(result.toString());
+        }
     }
 }
