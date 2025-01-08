10e11dadd937031d649acab9c39a5116d8dcfbf3
amit <amit1schneider@gmail.com>
2025-01-08 19:06:06 +0200

    calculator

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -1,42 +1,47 @@
 package com.example.simplecalculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.simplecalculator.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.Num1), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+
         });
     }
-    // Get view handle using the view id
 
 
+    @SuppressLint("SetTextI18n")
     public void onBtnClicked(View view) {
+
         EditText et1 = findViewById(R.id.Num1);
-
-        // Get the view’s text
         String et1Text = et1.getText().toString();
-
-        // Cast the String into Integer
         Integer num1 = Integer.valueOf(et1Text);
+
+        EditText et2 = findViewById(R.id.Num2);
+        String et2Text = et2.getText().toString();
+        Integer num2 = Integer.valueOf(et2Text);
+
         Integer result = null;
-        Integer num2;
         if (view.getId() == R.id.btnPlus)
             result = num1 + num2;
         if (view.getId() == R.id.btnMinus)
@@ -46,8 +51,11 @@ public class MainActivity extends AppCompatActivity {
         if (view.getId() == R.id.btnDiv)
             result = num1 / num2;
         if (result != null) {
-            TextView tvResult = findViewById(R.id.tvResult);
-            tvResult.setText(result.toString());
+            TextView tvRes = findViewById(R.id.tvResult);
+            tvRes.setText(result.toString());
         }
+
+
+
     }
 }
